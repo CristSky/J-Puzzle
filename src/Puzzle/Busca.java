53efd80aa40209c5bcc1204d4e3c6557b5064ab5
Cristiano <cristiano@live.jp>
2015-11-06 15:43:16 -0200

    j-puzzle-v2.1-final heuristic

--- src/Puzzle/Busca.java
+++ src/Puzzle/Busca.java
@@ -19,33 +19,26 @@ public class Busca {
 
     int limite = 5;
 
-    // testa a solução
+    //################### TESTA O OBJETIVO #########################################
     public boolean testeObjetivo(int[] base) {
         int objetivo[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 0};
         return Arrays.equals(base, objetivo);
     }
 
-    // H1 - peças fora do lugar
-    public int misplacedTiles(int[] array) {
-        int heuristic = 0;
-        for (int i = 0; i < array.length; i++) {
-            if ((i != (array[i] - 1)) && (array[i] != 0))
-                heuristic++;
+    //################### BUSCA EM LARGURA #########################################
+    public No buscaLargura(No raiz) {
+        list.add(raiz);
+        while (!list.isEmpty()) {
+            No aux = list.remove(0);
+            if (testeObjetivo(aux.estado))
+                return aux;
+            else
+                States.genStates(aux, list);
         }
-        return heuristic;
+        return raiz;
     }
 
-    // H2 - Algoritmo Manhattan distances
-    public int manhattanDistance(int[] array) {
-        int heuristic = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != 0)
-                heuristic += Math.abs((i / 3) - ((array[i] - 1) / 3)) + Math.abs((i % 3) - ((array[i] - 1) % 3));
-        }
-        return heuristic;
-    }
-
-    // busca em h com limite
+    //################### BUSCA EM PROFUNDIDADE LIMITADA ###########################
     public No buscaProfLimit(No raiz) {
         list.add(raiz);
         while (!list.isEmpty()) {
@@ -58,7 +51,7 @@ public class Busca {
         return raiz;
     }
 
-    // busca em h
+    //################### BUSCA EM PROFUNDIDADE ####################################
     public No buscaProf(No raiz) {
         list.add(raiz);
         while (!list.isEmpty()) {
@@ -71,7 +64,7 @@ public class Busca {
         return raiz;
     }
 
-    // busca IDA
+    //################### BUSCA DE APROFUNDAMENTO ITERATIVO ########################
     public No buscaIDA(No raiz) {
         list.add(raiz);
         while (!list.isEmpty()) {
@@ -91,27 +84,29 @@ public class Busca {
         return raiz;
     }
 
-    // busca em largura
-    public No buscaLargura(No raiz) {
-        list.add(raiz);
-        while (!list.isEmpty()) {
-            No aux = list.remove(0);
+    //################### BUSCA DE CUSTO UNIFORME ##################################
+    public No buscaCustoUniforme(No raiz) {
+        queue = new PriorityQueue<>(ComparatorH.getComparatorGH());
+        queue.add(raiz);
+        while (!queue.isEmpty()) {
+            No aux = queue.poll();
             if (testeObjetivo(aux.estado))
                 return aux;
             else
-                States.genStates(aux, list);
+                States.genStates(aux, queue);
         }
         return raiz;
     }
 
-    public No aStar(No raiz, int op) {
-        queue = new PriorityQueue<>(ComparatorH.getComparatorGH());
+    //################### BUSCA GME ################################################
+    public No buscaGME(No raiz, int op) {
+        queue = new PriorityQueue<>(ComparatorH.getComparatorH());
         No aux = raiz;
 
         if (op == 1)
-            raiz.setH(misplacedTiles(raiz.estado));
+            raiz.setH(States.getHeutistic(raiz.estado, 1)); // H1 - peças fora do lugar
         else
-            raiz.setH(manhattanDistance(raiz.estado));
+            raiz.setH(States.getHeutistic(raiz.estado, 2)); // H2 - Algoritmo Manhattan distances
 
         int distance = raiz.getH();
 
@@ -125,32 +120,51 @@ public class Busca {
         }
         return aux;
     }
+
+    //################### BUSCA A* #################################################
+    public No aStar(No raiz, int op) {
+        queue = new PriorityQueue<>(ComparatorH.getComparatorGH());
+        No aux = raiz;
+
+        if (op == 1)
+            raiz.setH(States.getHeutistic(raiz.estado, 1)); // H1 - peças fora do lugar
+        else
+            raiz.setH(States.getHeutistic(raiz.estado, 2)); // H2 - Algoritmo Manhattan distances
+
+        int distance = raiz.getH();
+
+        if (distance == 0)
+            return raiz;
+
+        while (distance != 0) {
+            States.genStates(aux, queue, op);
+            aux = queue.poll();
+            distance = aux.getH();
+        }
+        return aux;
+    }
 }
 
 class ComparatorH {
+    //################### COMPARATOR H #############################################
     public static Comparator<No> getComparatorH() {
-        return new Comparator<No>() {
-            @Override
-            public int compare(No o1, No o2) {
-                if (o1.getH() > o1.getH())
-                    return 1;
-                else if (o1.getH() < o1.getH())
-                    return -1;
-                return 0;
-            }
+        return (o1, o2) -> {
+            if (o1.getH() > o1.getH())
+                return 1;
+            else if (o1.getH() < o1.getH())
+                return -1;
+            return 0;
         };
     }
 
+    //################### COMPARATOR GH ############################################
     public static Comparator<No> getComparatorGH() {
-        return new Comparator<No>() {
-            @Override
-            public int compare(No o1, No o2) {
-                if (o1.getGH() > o2.getGH())
-                    return 1;
-                else if (o1.getGH() < o2.getGH())
-                    return -1;
-                return 0;
-            }
+        return (o1, o2) -> {
+            if (o1.getGH() > o2.getGH())
+                return 1;
+            else if (o1.getGH() < o2.getGH())
+                return -1;
+            return 0;
         };
     }
 }
--- src/Puzzle/Puzzle.java
+++ src/Puzzle/Puzzle.java
@@ -28,13 +28,13 @@ public class Puzzle {
         //int iniciar[] = new int[]{0, 2, 3, 7, 4, 1, 8, 6, 5}; //14
         //int iniciar[] = new int[]{2, 8, 3, 5, 0, 6, 1, 4, 7}; //14
         //int iniciar[] = new int[]{7, 4, 1, 8, 3, 2, 0, 5, 6}; //14
-        //int iniciar[] = new int[]{3, 5, 0, 2, 1, 7, 8, 4, 6}; //16
+        int iniciar[] = new int[]{3, 5, 0, 2, 1, 7, 8, 4, 6}; //16
         //int iniciar[] = new int[]{1, 5, 4, 8, 6, 2, 0, 7, 3}; //18
         //int iniciar[] = new int[]{4, 5, 3, 2, 0, 1, 7, 6, 8}; //20
         //int iniciar[] = new int[]{3, 4, 5, 8, 0, 6, 7, 1, 2}; //22
         //int iniciar[] = new int[]{0, 8, 6, 1, 7, 5, 2, 4, 3}; //24
         //int iniciar[] = new int[]{0, 8, 3, 7, 5, 2, 4, 6, 1}; //26
-        int iniciar[] = new int[]{6, 8, 7, 1, 4, 5, 0, 2, 3}; //28
+        //int iniciar[] = new int[]{6, 8, 7, 1, 4, 5, 0, 2, 3}; //28
         //int iniciar[] = new int[]{5, 2, 1, 3, 0, 4, 6, 8, 7}; //30
 
 
@@ -45,15 +45,17 @@ public class Puzzle {
         System.out.println("1- Busca em Largura   ");
         System.out.println("2- Busca em Profundidade Limitada");
         System.out.println("3- Busca em Profundidade");
-        System.out.println("4- Busca GME");
-        System.out.println("5- Busca A*");
+        System.out.println("4- Busca em Aprofundamento Iterativo");
+        System.out.println("5- Busca de Custo Uniforme");
+        System.out.println("6- Busca GME");
+        System.out.println("7- Busca A*");
 
-        opcao = 5;//input.nextInt();
-        if (opcao > 3) {
+        opcao = input.nextInt();
+        if (opcao > 5) {
             while (opcaoH < 1 || opcaoH > 2) {
                 System.out.println("1- H1 - misplacedTiles");
                 System.out.println("2- H2 - manhattanDistance");
-                opcaoH = 2;//input.nextInt();
+                opcaoH = input.nextInt();
             }
         }
 
@@ -64,22 +66,28 @@ public class Puzzle {
                 aux = find.buscaLargura(raiz);
                 break;
             case 2:
-                aux = find.buscaIDA(raiz);
+                aux = find.buscaProfLimit(raiz);
                 break;
             case 3:
                 aux = find.buscaProf(raiz);
                 break;
             case 4:
-                //TODO busca GME
+                aux = find.buscaIDA(raiz);
                 break;
             case 5:
+                aux = find.buscaCustoUniforme(raiz);
+                break;
+            case 6:
+                aux = find.buscaGME(raiz, opcaoH);
+                break;
+            case 7:
                 aux = find.aStar(raiz, opcaoH);
                 break;
         }
 
-        System.out.println("Inicial");
+        System.out.println("\nInicial");
         raiz.printEstado();
-        System.out.println("Final");
+        System.out.println("\nFinal");
         aux.printEstado();
         // empilha os nos para imprimir ordenado ascendente
         while (aux.pai != null) {
@@ -90,7 +98,7 @@ public class Puzzle {
 
         // imprime o caminho da solução
         long endTime = System.currentTimeMillis();
-        System.out.printf("Solução #time: %d ms\n", endTime - startTime);
+        System.out.printf("\nSolução #time: %d ms\n\n", endTime - startTime);
 
         while (arvore.size() != 0) {
             aux = arvore.remove(arvore.size() - 1);
